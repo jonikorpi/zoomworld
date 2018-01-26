efb5883884fdb4e2bfed73c1fd02c4985f53bb65
Joni Korpi <me@jonikorpi.com>
2018-01-26 13:02:15 +0200

    Adjusts test entity amount

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -10,10 +10,10 @@ import Graphic from "../components/Graphic";
 
 import { config, getSeed, baseTile, random } from "../utilities/graphics.js";
 
-const testTileRadius = 62;
+const testTileRadius = 100;
 const testEntityRadius = 10;
 const testTileCount = 75;
-const testEntityCount = 200;
+const testEntityCount = 300;
 
 const unit = 38.2;
 const xUnitType = "vmin";
