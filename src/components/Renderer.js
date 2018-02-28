4101f61cac271cb8f33ed64319f332d7f8c40ced
Joni Korpi <me@jonikorpi.com>
2018-02-28 11:17:58 +0200

    Adds a try/catch inside loop

--- src/components/Renderer.js
+++ src/components/Renderer.js
@@ -18,17 +18,26 @@ export default class Renderer extends React.Component {
   componentWillMount() {
     this.regl = startRegl(document.getElementById("canvas"));
     this.loop = this.regl.frame(context => {
-      const height = document.documentElement.clientHeight;
-      const scrolled = window.pageYOffset;
-      const scale = 1 - scrolled / height;
-      const time = performance.timing.navigationStart + performance.now();
-      const camera = positionAtTime(time, this.props.state, this.props.events);
+      try {
+        const height = document.documentElement.clientHeight;
+        const scrolled = window.pageYOffset;
+        const scale = 1 - scrolled / height;
+        const time = performance.timing.navigationStart + performance.now();
+        const camera = positionAtTime(
+          time,
+          this.props.state,
+          this.props.events
+        );
 
-      this.regl.clear(clearConfiguration);
+        this.regl.clear(clearConfiguration);
 
-      this.subscribers.forEach(callback => {
-        callback.call(callback, time, camera, scale);
-      });
+        this.subscribers.forEach(callback => {
+          callback.call(callback, time, camera, scale);
+        });
+      } catch (error) {
+        this.loop.cancel();
+        throw error;
+      }
     });
   }
 
